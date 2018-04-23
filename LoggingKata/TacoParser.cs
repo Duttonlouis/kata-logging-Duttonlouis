f03cb47e18050bcc5ab556379a83795a5cbd0e60
LouisDutton <Duttonlouis@gmail.com>
2018-04-23 08:21:55 -0500

    update had extra brackett

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,4 +1,5 @@
-﻿namespace LoggingKata
+﻿using System;
+namespace LoggingKata
 {
     /// <summary>
     /// Parses a POI file to locate all the TacoBells
@@ -6,13 +7,54 @@
     public class TacoParser
     {
         readonly ILog logger = new TacoLogger();
-        
+
         public ITrackable Parse(string line)
         {
             logger.LogInfo("Begin parsing");
 
-            //DO not fail if one record parsing fails, return null
-            return null; //TODO Implement
+            var cells = line.Split(',');
+
+            if (cells.Length < 3)
+            {
+                return null;
+            }
+            var longitude = 0.0;
+            var latitude = 0.0;
+            var name = "";
+
+            // grab the long from your array at index 0
+            // grab the lat from your array at index 1
+            // grab the name from your array at index 2
+            
+            
+             longitude = double.Parse(cells[0]);
+             latitude = double.Parse(cells[1]);
+             name = cells[2];
+
+
+
+
+
+            // Your going to need to parse your string as a `double`
+            // which is similar to parse a string as an `int`
+
+            // You'll need to create a TacoBell class
+            // that conforms to ITrackable
+
+            // Then, you'll need an instance of the TacoBell class
+            // With the name and point set correctly
+            var tacoBell = new Tacobell();
+            var point = new Point();
+            // Then, return the instance of your TacoBell class
+            // Since it conforms to ITrackable
+
+            
+            point.Longitude = longitude;
+            point.Latitude = latitude;
+            tacoBell.Location = point;
+            tacoBell.Name = name.Replace("\"","");
+            return tacoBell;
+
         }
     }
 }
