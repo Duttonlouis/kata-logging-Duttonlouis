5a691c0080a47431c1a4b823519f0e10ef607ec2
LouisDutton <Duttonlouis@gmail.com>
2018-04-23 12:17:19 -0500

    CW and RL

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -22,11 +22,53 @@ namespace LoggingKata
             var parser = new TacoParser();
 
             var locations = lines.Select(parser.Parse);
+            ITrackable a = null;
+            ITrackable b = null;
+            double distance = 0;
 
-            // TODO:  Find the two TacoBells in Alabama that are the furthurest from one another.
-            // HINT:  You'll need two nested forloops
+            foreach (var locA in locations)
+            {
+                var origin = new GeoCoordinate
+                {
+                    Latitude = locA.Location.Latitude,
+                    Longitude = locA.Location.Longitude,
+                };
+                foreach (var locB in locations)
+                {
+                    var destination = new GeoCoordinate
+                    {
+                        Latitude = locB.Location.Latitude,
+                        Longitude = locB.Location.Longitude,
+                    };
+                    var newDistance = origin.GetDistanceTo(destination);
+                    if (newDistance > distance)
+                    {
+                        a = locA;
+                        b = locB;
+                        distance = newDistance;
 
-            var cor = new GeoCoordinate(12, 9);
+                    }
+
+                }
+
+
+
+
+
+
+                // Do a loop for your locations to grab each location as the origin (perhaps: `locA`)
+                // Create a new corA Coordinate with your locA's lat and long
+
+                // Now, do another loop on the locations with the scope of your first loop, so you can grab the "destination" location (perhaps: `locB`)
+                // Create a new Coordinate with your locB's lat and long
+                // Now, compare the two using `origin.GetDistanceTo(distance)`, which returns a double
+                // If the distance is greater than the currently saved distance, update the distance and the two `ITrackable` variables you set above
+
+                // Once you've looped through everything, you've found the two Taco Bells furthest away from each other.
+            }
+            Console.WriteLine($"The Greatest distance between two tacobells is {a} and {b}.");
+            Console.WriteLine($"And the distance between them is {distance}.");
+            Console.ReadLine();
         }
     }
 }
